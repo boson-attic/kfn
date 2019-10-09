5d74cd2e13de3978a5670523bc14608fbd6b6a09
slinkydeveloper <francescoguard@gmail.com>
2019-10-09 16:37:16 +0200

    Added build-env and build-dev configuration entries for rust
    Signed-off-by: slinkydeveloper <francescoguard@gmail.com>

--- pkg/languages/rust/rust.go
+++ pkg/languages/rust/rust.go
@@ -6,18 +6,23 @@ import (
 	"github.com/containers/image/types"
 	"github.com/pelletier/go-toml"
 	"github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 	"github.com/slinkydeveloper/kfn/pkg/config"
 	"github.com/slinkydeveloper/kfn/pkg/image"
 	"github.com/slinkydeveloper/kfn/pkg/languages"
 	"github.com/slinkydeveloper/kfn/pkg/util"
 	"io/ioutil"
+	"os"
 	"os/exec"
 	"path"
+	"strconv"
 	"strings"
 )
 
 const (
 	rustRuntimeRemoteZip = "https://github.com/openshift-cloud-functions/faas-rust-runtime/archive/master.zip"
+	buildEnvVariables    = "build-env"
+	buildDevProfile      = "build-dev"
 )
 
 type rustLanguageManager struct {
@@ -130,19 +135,51 @@ func (r rustLanguageManager) ConfigureTargetDirectory(mainFile string, functionC
 }
 
 func (r rustLanguageManager) Compile(mainFile string, functionConfiguration map[string][]string) (string, []string, error) {
-	compileCommand := exec.Command("cargo", "build") // --release
+	devMode := false
+
+	// Check profile for building
+	dev, ok := functionConfiguration[buildDevProfile]
+	if ok {
+		var err error
+		devMode, err = strconv.ParseBool(dev[0])
+		if err != nil {
+			devMode = false
+		}
+	}
+
+	log.Printf("Using cargo dev profile: %v", devMode)
+
+	var compileCommand *exec.Cmd
+	if devMode {
+		compileCommand = exec.Command("cargo", "build")
+	} else {
+		compileCommand = exec.Command("cargo", "build", "--release")
+	}
 	// Root Cargo.toml is in runtime dir in runtime
 	compileCommand.Dir = path.Join(config.TargetDir, "runtime")
 	// Configure proper logging
 	compileCommand.Stdout = config.GetLoggerWriter()
 	compileCommand.Stderr = config.GetLoggerWriter()
+	compileCommand.Env = os.Environ()
+
+	envFlags, ok := functionConfiguration[buildEnvVariables]
+	if ok {
+		for _, env := range envFlags {
+			log.Printf("Adding env variable to cargo build: %s", env)
+			compileCommand.Env = append(compileCommand.Env, env)
+		}
+	}
 
 	err := compileCommand.Run()
 	if err != nil {
 		return "", nil, err
 	}
 
-	return path.Join(config.TargetDir, "runtime", "target", "debug" /* release */, "rust-faas"), nil, nil
+	if devMode {
+		return path.Join(config.TargetDir, "runtime", "target", "debug", "rust-faas"), nil, nil
+	} else {
+		return path.Join(config.TargetDir, "runtime", "target", "release", "rust-faas"), nil, nil
+	}
 }
 
 func (r rustLanguageManager) BuildImage(systemContext *types.SystemContext, imageName string, imageTag string, mainExecutable string, additionalFiles []string) (image.FunctionImage, error) {

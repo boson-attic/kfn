a7a8a0843302d70804c7a238794c3ede6cb1a278
slinkydeveloper <francescoguard@gmail.com>
2019-10-22 17:33:22 +0200

    Refactored rust example
    Signed-off-by: slinkydeveloper <francescoguard@gmail.com>

--- templates/rust/function.rs
+++ templates/rust/function.rs
@@ -1,22 +1,43 @@
 // kfn:dependency primal 0.2.3
-// kfn:dependency try_future 0.1.3
+// kfn:build-env RUSTC_WRAPPER=sccache
+// kfn:build-dev true
 
-use cloudevent::{Event, Reader};
-use serde_json::json;
+use cloudevent::{Event, Reader, Writer};
+use futures::IntoFuture;
 
 // Function must have this signature
-pub fn function(event: Option<Event>) -> Box<futures::Future<Item=Option<Event>, Error=actix_web::Error>> {
-    let input_json = try_future_box!(event.read_payload()).unwrap_or(serde_json::Value::Null);
-    let name = input_json
-        .as_object()
-        .and_then(|o| o.get("name"))
-        .and_then(|v| v.as_str())
-        .unwrap_or("World");
-    let json = json!({
-        "Hello": name,
-        "A_fibonacci_number": primal::StreamingSieve::nth_prime(10)
-    });
-    futures::finished(Some(json))
+pub fn function(
+    event: Option<Event>,
+) -> Box<dyn futures::Future<Item = Option<Event>, Error = actix_web::Error>> {
+    return match event.read_payload() {
+        // A payload is here
+        Some(Ok(p)) => Box::new(
+            p.as_u64()
+                // Payload is NaN
+                .ok_or(actix_web::error::ErrorBadRequest("Expecting a number"))
+                .map(|n| n as usize)
+                // Calculate the nth prime
+                .map(primal::StreamingSieve::nth_prime)
+                // Serialize the calculated number
+                .and_then(|n| {
+                    serde_json::value::to_value(n as u64)
+                        .map_err(actix_web::error::ErrorInternalServerError)
+                })
+                .and_then(|j| {
+                    event
+                        .unwrap()
+                        // From the actual event, create a new one with new payload
+                        .clone_with_new_payload("application/json", j)
+                        .map(Option::from)
+                        .map_err(actix_web::error::ErrorInternalServerError)
+                })
+                .into_future(),
+        ),
+        // Payload is malformed
+        Some(Err(e)) => Box::new(futures::failed(actix_web::error::ErrorBadRequest(e))),
+        // No payload at all
+        None => Box::new(futures::failed(actix_web::error::ErrorBadRequest(
+            "Expecting a non empty json payload",
+        ))),
+    };
 }
-
-//TODO refactror this shitting example
